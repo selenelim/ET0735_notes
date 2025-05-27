d31d46a6e78a1de05c56fb36b599842486a97b5d
selenelim <bobofishballg@gmail.com>
2025-05-27 22:57:28 +0800

    new

--- stringtofloat.py
+++ stringtofloat.py
@@ -2,3 +2,12 @@ price_str = "3.1415"
 price_float = float(price_str)
 
 print(price_float)  # Output: 3.1415
+
+
+value = 3.141592
+rounded = round(value, 2)  # 2 decimal places
+print(rounded)  # Output: 3.14
+
+value = 3.141592
+rounded = round(value, 2)  # 2 decimal places
+print(rounded)  # Output: 3.14
